603c64f14812d4fec9557ff9236b4f89c3ee2f17
Ravi Yadav <ravi.knr1116@gmail.com>
2026-07-25 01:40:04 +0530

    Time: 4 ms (44.49%), Space: 18.1 MB (45.89%) - LeetHub

--- 0692-top-k-frequent-words/0692-top-k-frequent-words.cpp
+++ 0692-top-k-frequent-words/0692-top-k-frequent-words.cpp
@@ -1,59 +1,52 @@
 class Solution {
 public:
      
-       struct cmp {
-        bool operator()(pair<int, string> &a, pair<int, string> &b) {
-            if (a.first != b.first) {
-                 return a.first > b.first;  
-            }
-          return a.second < b.second;  
-        }
-    };
+      struct cmp{
+        bool operator()(pair<int,string>&a,pair<int,string>&b){
+         if(a.first!=b.first){
+           return a.first>b.first;
+         }
+         return a.second<b.second;
+         }
+      };
 
     vector<string> topKFrequent(vector<string>& words, int k) {
-          unordered_map<string,int>f;
-          vector<string>v;
-          priority_queue<pair<int,string>,vector<pair<int,string>>,cmp>pq;
-        for(int i=0;i<words.size();i++){
-          f[words[i]]++;
-        }
-        
-        for(auto it:f){
-          int frequency = it.second;
-          string element = it.first;
-           pair<int,string>curr={frequency,element};
-           
-           if(pq.size()<k){
-            pq.push(curr);
+            unordered_map<string,int>f;
+      vector<string>v;
+      priority_queue<pair<int,string>,vector<pair<int,string>>,cmp>pq;
+      for(int i=0;i<words.size();i++){
+        f[words[i]]++;
+      }
+      for(auto it:f){
+        int frequency = it.second;
+        string value = it.first;
+        if(pq.size()<k){
+            pq.push({frequency,value});
             continue;
-           }
-           else if(frequency==pq.top().first){
-             if(pq.top().second>element){
-                    pq.pop();
-               pq.push(curr);
-               continue;
-             }
-             else{
-              continue;
-             }
-           }
-           else if(frequency<pq.top().first){
+        }
+        else if(pq.top().first>frequency){
+           continue;
+        }
+        else if(pq.top().first==frequency){
+            if(pq.top().second>value){
+                pq.pop();
+            pq.push({frequency,value});
+            }
+            else{
+                continue;
+            }
+        }
+        else{
+             pq.pop();
+             pq.push({frequency,value});
              continue;
-           }
-           else{
-               pq.pop();
-               pq.push(curr);
-               continue;
-           }
         }
-
-        while(!pq.empty()){
-          v.push_back(pq.top().second);
-          pq.pop();
-        }
-
-       reverse(v.begin(),v.end());
-
-        return v;
+      }
+      while(!pq.empty()){
+        v.push_back(pq.top().second);
+        pq.pop();
+      }
+      reverse(v.begin(),v.end());
+      return v;
     }
 };
