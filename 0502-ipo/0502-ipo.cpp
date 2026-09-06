719e2a7d016061bd911040f12578b1ab63c8c0c4
Ravi Yadav <ravi.knr1116@gmail.com>
2026-09-06 17:59:33 +0530

    Time: 72 ms (66.48%), Space: 131.5 MB (49.84%) - LeetHub

---
+++ 0502-ipo/0502-ipo.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int findMaximizedCapital(int k, int w, vector<int>& profits, vector<int>& capital) {
+         vector<pair<int,int>>v;
+         for(int i=0;i<profits.size();i++){
+            v.push_back({capital[i],profits[i]});
+         }
+          sort(v.begin(),v.end());
+
+          priority_queue<int>pq;
+
+            int index=0;
+          for(int i=0;i<k;i++){
+            
+            while(index<v.size() && w>=v[index].first){
+                pq.push(v[index].second);
+                
+                index++;
+            }
+                     if(pq.empty()){
+                   break;
+                }
+
+                w+=pq.top();
+                pq.pop();
+
+          }
+       
+          
+          return w;
+    }
+};
