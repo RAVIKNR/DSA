17e3f12b721aacd11a7aec19ae83fc499c3d807c
Ravi Yadav <ravi.knr1116@gmail.com>
2026-09-15 23:13:48 +0530

    Time: 4 ms (30.66%), Space: 16.4 MB (15.1%) - LeetHub

---
+++ 0022-generate-parentheses/0022-generate-parentheses.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    vector<string> generateParenthesis(int n) {
+        int open=0;
+        int close=0;
+        string temp;
+        vector<string>ans;
+          gen(open,close,temp,ans,n);
+         return ans;
+    }
+
+   void gen(int open,int close,string temp,vector<string> &ans,int n){
+        if(open==n && close==n){
+           ans.push_back(temp);
+           return;
+        }
+
+        if(open<n){
+            temp.push_back('(');
+            gen(open+1,close,temp,ans,n);
+            temp.pop_back();
+        }
+
+        if(close<open){
+            temp.push_back(')');
+            gen(open,close+1,temp,ans,n);
+            temp.pop_back();
+        }
+    }
+};
